c885e10a7d5c8b28dac4d8a96b5786be8f3dd4b7
Leilane <53538519+noleilane@users.noreply.github.com>
2021-02-10 21:51:41 -0300

    Nova atualização do index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <h1>
      <?php echo "Leilane Oliveira do Nascimento"; ?>
     </h1>
+    <img src="https://www.oversodoinverso.com.br/wp-content/uploads/2018/09/0-30.jpg" alt="Mulher branca segura galinha fofa" width="500" height="600">
   </body>
 
 </html>
